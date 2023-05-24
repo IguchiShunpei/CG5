c4aa7e56f1ee5dd5acf70efc11b677df640d2cbe
IguchiShunpei <k021g1129@g.neec.ac.jp>
2023-05-24 12:55:07 +0900

    shiftBlurとgaussianBlurと縮小バッファの実装

--- Assets/PostEffectAttacher.cs
+++ Assets/PostEffectAttacher.cs
@@ -7,6 +7,7 @@ public class PostEffectAttacher : MonoBehaviour
     public Shader shader;
     private Material materialA;
     private Material materialB;
+
     private void Awake()
     {
         materialA = new Material(shader);//shaderを割り当てたマテリルの動的生成   
@@ -32,9 +33,28 @@ public class PostEffectAttacher : MonoBehaviour
             );
         Graphics.Blit(source, tmp, materialA);
         Graphics.Blit(tmp, destination, materialB);
-
         //必ずリリース
         RenderTexture.ReleaseTemporary(tmp);
         RenderTexture.ReleaseTemporary(tmp2);
+
+        //縮小バッファ
+        RenderTexture buff1 = RenderTexture.GetTemporary(source.width / 2, source.height / 2, 0, source.format);
+        RenderTexture buff2 = RenderTexture.GetTemporary(source.width / 4, source.height / 4, 0, source.format);
+        RenderTexture buff3 = RenderTexture.GetTemporary(source.width / 8, source.height / 8, 0, source.format);
+        //シェーダ適応用バッファ。一番小さいサイズのバッファと同じサイズで確保
+        RenderTexture blurTex = RenderTexture.GetTemporary(buff3.width,buff2.height,0,buff3.format);
+
+        Graphics.Blit(source, buff1);
+        Graphics.Blit(buff1, buff2);
+        Graphics.Blit(buff1, buff3);
+        Graphics.Blit(buff3, blurTex);
+        Graphics.Blit(buff2, buff1);
+        Graphics.Blit(buff3, buff1);
+        Graphics.Blit(buff1,destination);
+
+        RenderTexture.ReleaseTemporary(buff1);
+        RenderTexture.ReleaseTemporary(buff2);
+        RenderTexture.ReleaseTemporary(buff3);
+        RenderTexture.ReleaseTemporary(blurTex);
     }
 }
